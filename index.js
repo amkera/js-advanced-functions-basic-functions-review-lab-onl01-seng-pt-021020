0c2d716163d1d7c6ea2e6121b51b9a88205aca36
Amber Kerawala Torres <amber_kerawala@berkeley.edu>
2020-09-01 20:37:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
 function saturdayFun(activity="roller-skate") {
   console.log(`This Saturday, I want to ${activity}!`);
 }
+saturdayFun("bathe my dog")
 
 function mondayWork(activity="go to the office") {
   console.log(`This Monday, I will ${activity}.`);
 }
-
+mondayWork("work from home")
